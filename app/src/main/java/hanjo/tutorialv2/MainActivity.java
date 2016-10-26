9325108cee4204956538c2c8a95aeb8d766015a4
Bubbler-4 <bubbler9903@gmail.com>
2016-10-26 19:33:58 +0900

    Add one on click

--- app/src/main/java/hanjo/tutorialv2/MainActivity.java
+++ app/src/main/java/hanjo/tutorialv2/MainActivity.java
@@ -2,10 +2,13 @@ package hanjo.tutorialv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button button;
     private TextView textView;
@@ -22,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
         i = 0;
-        textView.setText(i.toString());
+        textView.setText(String.format(Locale.US, "%d", i));
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        i += 1;
+        textView.setText(String.format(Locale.US, "%d", i));
     }
 }

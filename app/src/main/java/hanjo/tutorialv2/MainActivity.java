e3f6eb37c0a73639e94854e4fd8fbda92716df82
Bubbler-4 <bubbler9903@gmail.com>
2016-10-26 18:11:53 +0900

    Add button

--- app/src/main/java/hanjo/tutorialv2/MainActivity.java
+++ app/src/main/java/hanjo/tutorialv2/MainActivity.java
@@ -2,12 +2,20 @@ package hanjo.tutorialv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        button = (Button) findViewById(R.id.button);
     }
 }
